5823b5688d08e7c458c1d175afa8ab36d06409ec
MarlaRey <hejmarlenehej@gmail.com>
2024-04-15 09:26:48 +0200

    grid

--- src/components/Blog/BlogList.jsx
+++ src/components/Blog/BlogList.jsx
@@ -5,27 +5,24 @@ import styles from './BlogList.module.scss';
 
 const BlogList = ({ categoryList }) => {
   const allBlogPosts = useBlogPosts(); // Hent alle blogposter
-  
 
-const filteredBlogPosts = allBlogPosts.filter(post => {
-  // Hvis kategorien er "Alle", vises alle blogposter
-  if (!categoryList || categoryList === "Alle") {
-    return true;
-  }
-  // Sikre, at post.fields.categoryList er defineret før filtrering
-  if (post.fields.categoryList && post.fields.categoryList.includes(categoryList)) {
-    return true;
-  }
-  return false;
-});
-
-
+  const filteredBlogPosts = allBlogPosts.filter(post => {
+    // Hvis kategorien er "Alle", vises alle blogposter
+    if (!categoryList || categoryList === "Alle") {
+      return true;
+    }
+    // Sikre, at post.fields.categoryList er defineret før filtrering
+    if (post.fields.categoryList && post.fields.categoryList.includes(categoryList)) {
+      return true;
+    }
+    return false;
+  });
 
   return (
-    <div>
+    <div className={styles.blogGrid}>
       {/* Vis de filtrerede blogposter */}
-      {filteredBlogPosts.map((post) => (
-        <div className="blog-post" key={post.sys.id}>
+      {filteredBlogPosts.map((post, index) => (
+        <div className={styles.blogPost} key={post.sys.id} style={{ gridArea: getGridArea(index) }}>
           {post.fields.media && post.fields.media.fields.file && (
             <img src={post.fields.media.fields.file.url} alt={post.fields.title} />
           )}
@@ -34,11 +31,35 @@ const filteredBlogPosts = allBlogPosts.filter(post => {
           <h2>{post.fields.title}</h2>
           <p>{post.fields.text}</p>
           <p>{post.fields.categoryList}</p>
-          <Link to={`/blogDetails/${post.sys.id}`} className='readMore'>Read more</Link>
+          <Link to={`/blogDetails/${post.sys.id}`} className={styles.readMore}>Read more</Link>
         </div>
       ))}
     </div>
   );
+};
+
+// Funktion til at returnere gridArea baseret på index
+const getGridArea = (index) => {
+  switch (index) {
+    case 0:
+      return 'a';
+    case 1:
+      return 'b';
+    case 2:
+      return 'c';
+    case 3:
+      return 'd';
+    case 4:
+      return 'e';
+    case 5:
+      return 'f';
+    case 6:
+      return 'g';
+    case 7:
+      return 'h';
+    case 8:
+      return 'i';
+  }
 };
 
 export default BlogList;
